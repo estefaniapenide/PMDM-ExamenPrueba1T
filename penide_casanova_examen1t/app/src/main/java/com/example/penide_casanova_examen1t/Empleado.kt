a58115ee9f04cb91cc018c8c0c420d1d80e65cf7
etef <estefaniapenide@gmail.com>
2021-11-11 20:49:10 +0100

    Pruebas herencia

---
+++ penide_casanova_examen1t/app/src/main/java/com/example/penide_casanova_examen1t/Empleado.kt
@@ -0,0 +1,13 @@
+package com.example.penide_casanova_examen1t
+
+open class Empleado(nombre:String,apellido1:String,apellido2:String,sexo:String,fecha:Int) {
+
+}
+
+class Comercial():Empleado(){
+
+}
+
+class Programador:Empleado(){
+
+}
